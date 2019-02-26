eca87441e993df71b1bdca695df7d43a3e7ef896
Andres Escobar Cotan <aescobac@everis.com>
2019-02-26 16:23:33 +0100

    Charla vision computacional

--- ejercicioResueltoContornos.py
+++ ejercicioResueltoContornos.py
@@ -1,19 +1,24 @@
 import numpy as np
 import cv2
 
+CANAL_SELECCIONADO = 0
 KERNEL = (11, 11) # KERNEL del gausiano esta a 11 
 mk = np.ones((51,51),np.uint8) # Elemento morfologico
-cap = cv2.VideoCapture('cochesPasando.mp4') # Abre el video
+cap = cv2.VideoCapture('camaraSeguridad.mp4') # Abre el video
 background_frame = 0
 _, frame = cap.read() # Devuelve un frame, la _ indica que se rechaza el segundo parametro
 background_frame = np.copy(frame) # Copia el frame en una matriz nueva
-background_frame = cv2.cvtColor(background_frame, cv2.COLOR_BGR2GRAY) # Se convierte a blanco y negro 
+#background_frame = cv2.cvtColor(background_frame, cv2.COLOR_BGR2GRAY) # Se convierte a blanco y negro 
+background_frame = np.copy(frame[:,:,CANAL_SELECCIONADO])
+background_frame = cv2.equalizeHist(background_frame)
 background_frame = cv2.GaussianBlur(background_frame,KERNEL, 0) # Se aplica un gausiano para eliminar el ruido
 cv2.imshow('fondo', background_frame)     # Se muestra el fondo
 
 while(cap.isOpened()): # Mientras haya frames
     _, frame = cap.read() # Se lee el frame
-    gray_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY) # Se copia en blanco y negro (por valor) el frame
+    gray_frame = np.copy(frame[:,:,CANAL_SELECCIONADO])
+    #gray_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY) # Se copia en blanco y negro (por valor) el frame
+    gray_frame = cv2.equalizeHist(gray_frame)
     gray_frame =  cv2.GaussianBlur(gray_frame,KERNEL, 0) # Se aplica un gaussiano
     diff = cv2.absdiff(background_frame, gray_frame) # IMPORTANTE se resta el fondo, con el frame actual
     canny = cv2.Canny(diff, 10, 20, 3) # Se hallan los bordes, usando el metodo de Canny
