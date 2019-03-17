e46180c8fd1da084ebec907c6590f42fdc92f207
Andrés Escobar Cotan <andres.escobar.cotan@gmail.com>
2019-03-17 21:21:48 +0100

    Arreglado ejercicio sin resolver

--- ejercicioSinResolver.py
+++ ejercicioSinResolver.py
@@ -25,18 +25,7 @@ _, frame = cap.read() # Devuelve un frame, la _ indica que se rechaza el segundo
 
 while(cap.isOpened()): # Mientras haya frames
     _, frame = cap.read() # Se lee el frame
-    # Extraer el color de la crrretera
-    carretera = np.copy(frame)
-    # BGR -> OPENCV 0 azul, 1 verde, 2 rojo
-    _, color_b = scipy.stats.mode(carretera[:,:,0])
-    _, color_g = scipy.stats.mode(carretera[:,:,1])
-    _, color_r = scipy.stats.mode(carretera[:,:,2])
-    print(color_b.shape)
-    #carretera[:, :, 0] = carretera[:,:,0] - color_b[0,:]
-    #carretera[:, :, 1] = carretera[:,:,1] - color_g[0,:]
-    #carretera[:, :, 2] = carretera[:,:,2] - color_r[0,:]
-    carretera[:] = carretera[:] - np.transpose([color_b[0,:], color_g[0,:], color_r[0,:]])
-    cv2.imshow('final',carretera) # Se muestra en la ventana con nombre final, y la matriz frame
+    cv2.imshow('final',frame) # Se muestra en la ventana con nombre final, y la matriz frame
      
     if cv2.waitKey(1) & 0xFF == ord('q'): # Si se pulsa la tecla q se sale del bucle
         break
